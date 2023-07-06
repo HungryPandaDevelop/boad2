22488e6f29f32df6274e8bf44a8906d7fa45a84a
panda <bink@inbox.ru>
2023-07-06 11:28:26 +0300

    save

--- js/sourse/common-parts/phone-flag.js
+++ js/sourse/common-parts/phone-flag.js
@@ -3,12 +3,13 @@ function phone_mask(){
 	$.mask.definitions['9']='';
 	$.mask.definitions['d']='[0-9]';
   
-	$('.phone').mask('+7 ddd ddd-dd-dd');
+	// $('.phone').mask('+7 ddd ddd-dd-dd');
+	$('.phone').mask('+971 ddd ddd-dddd');
 	$('.phone').intlTelInput({
 		autoHideDialCode:false,
 		autoPlaceholder:'aggressive',
 		placeholderNumberType:'MOBILE',
-		preferredCountries:['ru','ae'],
+		preferredCountries:['ae','ru'],
 		separateDialCode:true,
 		utilsScript:'/wp-content/themes/pandadev/frontend/js/sourse/other-js/phone/utils.js',
 		// utilsScript:'../js/sourse/other-js/phone/utils.js',
@@ -21,15 +22,15 @@ function phone_mask(){
 		//dropdownContainer:document.body,
 		//excludeCountries:['us'],
 		//formatOnDisplay:false,
-		geoIpLookup:function(callback,){
-			$.get('http://ipinfo.io',function(){},'jsonp').always(function(resp){
-				var countryCode =(resp&&resp.country)?resp.country:'';
+		// geoIpLookup:function(callback,){
+		// 	$.get('http://ipinfo.io',function(){},'jsonp').always(function(resp){
+		// 		var countryCode =(resp&&resp.country)?resp.country:'';
 				
-				callback(countryCode);
-				// console.log('selectedCountryData 2', selectedCountryData, selectedCountryPlaceholder)
+		// 		callback(countryCode);
+		// 		// console.log('selectedCountryData 2', selectedCountryData, selectedCountryPlaceholder)
 				
-			});
-		},
+		// 	});
+		// },
 		//hiddenInput:'full_number',
 		//initialCountry:'auto',
 		//localizedCountries:{'de':'Deutschland'},
