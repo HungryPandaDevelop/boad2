6715685f781dd6d360c0f87cba4c1209737f4960
HungryPanda <hungrypandadevelop@gmail.com>
2024-05-08 17:20:46 +0300

    save

--- js/sourse/common-parts/search-popup.js
+++ js/sourse/common-parts/search-popup.js
@@ -1,11 +1,12 @@
-const ajaxSeatch = (searchVal)=>{
+const ajaxSeatch = (searchVal) => {
 
+  let lang = $('.search-main').data('lang');
 
   $.ajax({
     type: "GET",
     url: "/wp-json/search/all",
-    data: {'search': searchVal},
-    success: function(result){
+    data: { 'search': searchVal, lang: lang },
+    success: function (result) {
       console.log('run', result);
 
       generateContent(result);
@@ -17,36 +18,36 @@ let mainBox = $('.search-popup');
 
 
 
-const generateContent = async (result)=>{
+const generateContent = async (result) => {
 
 
   mainBox.empty();
 
 
 
-  let templateHtml = (linkNum, titleNum, list)=>{
+  let templateHtml = (linkNum, titleNum, list) => {
 
-    let linkMass = ['yachts', 'services','routes', 'blog'];
-    let titleMass = ['Яхты', 'Услуги','Маршруты', 'Блог'];
-    
-    return(`
+    let linkMass = ['yachts', 'services', 'routes', 'blog'];
+    let titleMass = ['Яхты', 'Услуги', 'Маршруты', 'Блог'];
+
+    return (`
     <div class="search-popup-line">
           <h3> <a href="${linkMass[linkNum]}">${titleMass[titleNum]}</a></h3>
           <ul class="ln">
-            ${list.map(item=>`<li><a href="${item.link}">${item.title}</a></li>`).join('')}
+            ${list.map(item => `<li><a href="${item.link}">${item.title}</a></li>`).join('')}
           </ul>
         </div>
     `)
-  } ;
+  };
 
-  result.yachts.length > 0 && (mainBox.append(templateHtml(0,0,result.yachts)));
-  result.services.length > 0 && (mainBox.append(templateHtml(1,1,result.services)));
-  result.routes.length > 0 && (mainBox.append(templateHtml(2,2,result.routes)));
-  result.blog.length > 0 && (mainBox.append(templateHtml(3,3,result.blog)));
+  result.yachts.length > 0 && (mainBox.append(templateHtml(0, 0, result.yachts)));
+  result.services.length > 0 && (mainBox.append(templateHtml(1, 1, result.services)));
+  result.routes.length > 0 && (mainBox.append(templateHtml(2, 2, result.routes)));
+  result.blog.length > 0 && (mainBox.append(templateHtml(3, 3, result.blog)));
 
   let resCount = result.yachts.length + result.services.length + result.routes.length + result.blog.length;
 
-  if  ( resCount === 0){
+  if (resCount === 0) {
     mainBox.append('<div class="empty-list">Список пуст</div>');
   }
 }
@@ -54,44 +55,44 @@ const generateContent = async (result)=>{
 let result = {};
 let searchTimeId;
 
-$('.input-search').on('keyup',function(){
+$('.input-search').on('keyup', function () {
   let inputLength = $(this).val().length;
-  if(inputLength>0){
+  if (inputLength > 0) {
     $(this).parents('.search-main').addClass('search-on');
-  }else{
+  } else {
     $(this).parents('.search-main').removeClass('search-on');
   }
 });
 
-$('.search-main').on('click','.close-ico',function(){
+$('.search-main').on('click', '.close-ico', function () {
   $(this).parents('.search-main').removeClass('search-on');
   $(this).parents('.search-main').find('.input-search').val('');
   mainBox.removeClass('active');
 });
 
-$('.input-search-ajax').on('keyup',function(){
+$('.input-search-ajax').on('keyup', function () {
 
   let searchVal = $(this).val();
-  
 
-  if(searchVal.length>0){
+
+  if (searchVal.length > 0) {
     mainBox.addClass('active');
     mainBox.append(spinner);
     clearTimeout(searchTimeId);
 
-    searchTimeId = setTimeout(function(){
-      
+    searchTimeId = setTimeout(function () {
+
       ajaxSeatch(searchVal);
 
     }, 2000);
 
 
-  }else{
+  } else {
     mainBox.removeClass('active');
 
   }
 
-  
+
 
 
 });
